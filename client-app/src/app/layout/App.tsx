02c994effedb63b0d8dee0846ca891e154e17810
Afiq <muhammad.afiq@gosaas.io>
2024-10-23 12:02:06 +0800

    End section4

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,7 +5,6 @@ import { Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
-
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities')
       .then(response => {
